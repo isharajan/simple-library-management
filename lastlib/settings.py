8efc69cf05d15d87723deed50d9716c9df5e9539
kathirraja48 <kathirraja48@gmail.com>
2020-07-01 19:01:39 +0530

    updates in settings

--- lastlib/settings.py
+++ lastlib/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 print (">>>>>>>>>>>>>>>>>>>>>>")
 print(DATABASES)
@@ -124,6 +124,6 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
-
-STATICFILES_DIRS = [STATIC_ROOT]
+MEDIA_URL ='/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
